aabdc39299936a2d46c3d40ec3955591668f0bcb
William Bay <william@williambay.com>
2017-05-06 17:21:45 -0700

    Adds custom fields for Tutorial post type.

--- functions.php
+++ functions.php
@@ -398,3 +398,70 @@ function fs_next_post_links(){ ?>
 
 <?php }
 
+
+
+
+
+
+/**
+ * Tutorial custom fields
+ * 
+ */
+
+
+if( function_exists('acf_add_local_field_group') ):
+
+acf_add_local_field_group(array (
+	'key' => 'group_590d7ae211558',
+	'title' => 'Tutorials',
+	'fields' => array (
+		array (
+			'key' => 'field_590d7af223392',
+			'label' => 'Video URL',
+			'name' => 'fs_video_url',
+			'type' => 'oembed',
+			'instructions' => '',
+			'required' => 0,
+			'conditional_logic' => 0,
+			'wrapper' => array (
+				'width' => '',
+				'class' => '',
+				'id' => '',
+			),
+			'width' => '',
+			'height' => '',
+		),
+	),
+	'location' => array (
+		array (
+			array (
+				'param' => 'post_type',
+				'operator' => '==',
+				'value' => 'tutorials',
+			),
+		),
+	),
+	'menu_order' => 0,
+	'position' => 'normal',
+	'style' => 'default',
+	'label_placement' => 'top',
+	'instruction_placement' => 'label',
+	'hide_on_screen' => array (
+		0 => 'permalink',
+		1 => 'excerpt',
+		2 => 'custom_fields',
+		3 => 'discussion',
+		4 => 'comments',
+		5 => 'author',
+		6 => 'format',
+		7 => 'page_attributes',
+		8 => 'featured_image',
+		9 => 'categories',
+		10 => 'tags',
+		11 => 'send-trackbacks',
+	),
+	'active' => 1,
+	'description' => '',
+));
+
+endif;
