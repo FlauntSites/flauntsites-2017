65877939adbabfda0c9d524e3d48ca928721aefe
William Bay <william@williambay.com>
2017-07-11 15:21:26 -0700

    Fixed an issue with the Tutorial CPT not showing up in Admin

--- functions.php
+++ functions.php
@@ -262,7 +262,7 @@ function fs_tutorials_post_type() {
 		'public'                => true,
 		'show_ui'               => true,
 		'show_in_menu'          => true,
-		'menu_position'         => 5,
+		'menu_position'         => 10,
 		'show_in_admin_bar'     => true,
 		'show_in_nav_menus'     => true,
 		'can_export'            => true,
@@ -305,13 +305,6 @@ register_taxonomy( 'lessons',
 		)
 	);
 
-
-
-
-/**
- * Forces the Tutorial plugin to be Private by default.
- * 
- */
 
 
 
