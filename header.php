8340f6a12588cccc19254c79199f0f0b6cffeae4
William Bay <william@flauntyoursite.com>
2018-07-14 00:06:25 -0700

    Added Hamburger Icon. Fixed Styling for Main menu Icon - Mobile size.

--- header.php
+++ header.php
@@ -39,7 +39,7 @@
 			</div>
 
 			<nav id="site-navigation" class="main-navigation" role="navigation">
-				<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><i class="fa fa-bars"></i><?php esc_html_e( 'Menu', 'flauntsites2017' ); ?></button>
+				<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><div class="hamburger"><?php require( 'images/menu-hamburger.svg' ); ?></div><?php esc_html_e( 'Menu', 'flauntsites2017' ); ?></button>
 				<?php wp_nav_menu( array( 'theme_location' => 'menu-1', 'menu_id' => 'primary-menu' ) ); ?>
 			</nav><!-- #site-navigation -->
 
